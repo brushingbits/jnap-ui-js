01cf08836067bea7f98ba1518e9b44d61d8b357c
Daniel Rochetti <daniel.rochetti@gmail.com>
2010-11-18 23:53:09 -0200

    encoded html entities on strings

--- src/i18n/ext-jnap-lang-pt_BR.js
+++ src/i18n/ext-jnap-lang-pt_BR.js
@@ -10,21 +10,26 @@
 		_ns.NotificationMessage.prototype.closeTitle = 'Fechar mensagem';
 	}
 
+	// Rating
+	if (_ns.Rating) {
+		_ns.Rating.prototype.clearText = 'Limpar minha avalia&ccedil;&atilde;o';
+	}
+
 	// CharactersLimitPlugin
 	if (_ns.form.CharactersLimitPlugin) {
-		_ns.form.CharactersLimitPlugin.prototype.text = 'Você possui <strong>{0}</strong> caracteres restantes';
+		_ns.form.CharactersLimitPlugin.prototype.text = 'Voc&ecirc; possui &lt;strong&gt;{0}&lt;/strong&gt; caracteres restantes';
 	}
 
 	// FormUtils
 	if (_ns.form.FormUtils) {
 		_ns.form.FormUtils.defaultSuccessMsg = 'Seus dados foram enviados com sucesso.';
-		_ns.form.FormUtils.defaultInvalidMsg = 'Seu formulário contém dados inválidos, corrija-os e tente novamente.';
-		_ns.form.FormUtils.defaultErrorMsg = 'Houve um erro de comunicação com o servidor no processamento do seu formulário';
+		_ns.form.FormUtils.defaultInvalidMsg = 'Seu formul&aacute;rio cont&eacute;m dados inv&aacute;lidos, corrija-os e tente novamente.';
+		_ns.form.FormUtils.defaultErrorMsg = 'Houve um erro de comunica&ccedil;&atilde;o com o servidor no processamento do seu formul&aacute;rio';
 	}
 
 	// RequiredFieldsPlugin
 	if (_ns.form.RequiredFieldsPlugin) {
-		_ns.form.RequiredFieldsPlugin.prototype.hintText = 'Os campos marcados com {0} são obrigatórios';
+		_ns.form.RequiredFieldsPlugin.prototype.hintText = 'Os campos marcados com {0} s&atilde;o obrigat&oacute;rios';
 	}
 
 })();
